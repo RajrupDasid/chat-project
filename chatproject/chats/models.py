3abe467fb90e916ab1ef559af194a48322ac1dfd
RajrupDasid <rajrupdasid@outlook.com>
2021-07-25 16:35:32 +0530

    Hobby project

---
+++ chatproject/chats/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Chat(models.Model):
+    room_name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.room_name)
+    def get_messages(self):
+        return self.messages.all()
---
+++ chatproject/chats_messages/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from chats.models import Chat
+# Create your models here.
+class ChatMessage(models.Model):
+    chat=models.ForeignKey(Chat,on_delete=models.CASCADE,related_name="messages")
+    nick=models.CharField(max_length=200)
+    text=models.TextField()
+
+    def __str(self):
+        return f"{self.chat.room_name}-{self.nick}"
